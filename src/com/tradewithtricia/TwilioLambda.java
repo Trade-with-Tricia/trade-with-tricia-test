907e255d69af7c5f1c61a058f452d1be7f41387c
AWilliams18 <williaan18@up.edu>
2018-06-05 11:57:28 -0700

    Reading JSON basics

--- src/com/tradewithtricia/TwilioLambda.java
+++ src/com/tradewithtricia/TwilioLambda.java
@@ -1,7 +1,9 @@
 package com.tradewithtricia;
 
 //TODO: thin out these imports
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
@@ -17,18 +19,24 @@ import java.util.Map;
 
 
 
-public class TriciaLambda implements RequestHandler<Request, Response> {
+public class TwilioLambda implements RequestHandler<Map<String, Object>, Response> {
 
     String TAC = "By replying to this message, you agree to the Terms and Conditions.";
-    AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
+    AmazonDynamoDB client = AmazonDynamoDBClientBuilder
+            .standard()
+            //.withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+            .withRegion(Regions.US_EAST_1)
+            .build();
     DynamoDB dynamo = new DynamoDB(client);
 
     @Override
-    public Response handleRequest(Request request, Context context) {
+    public Response handleRequest(Map<String, Object> request, Context context) {
 
         //todo: handle errors gracefully
 
         //todo: the rest of it
+        String SMS = (String) request.get("body-json");
+        System.out.println(SMS);
 
 
         return null;
